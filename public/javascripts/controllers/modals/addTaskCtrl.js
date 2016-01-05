1487ca66edbdc31650a09826d84655a1f0fad4e0
Alin Mindroc <mindroc.alin@gmail.com>
2016-01-05 20:09:34 +0200

    changed projects and tasks page interfaces

--- public/javascripts/controllers/modals/addTaskCtrl.js
+++ public/javascripts/controllers/modals/addTaskCtrl.js
@@ -1,13 +1,14 @@
 angular.module('taskManagerApp')
-.controller('addTaskCtrl', function ($scope, $uibModalInstance, Projects, Users, Tasks, CRUD, currentProject) {
-	$scope.project = currentProject;
+.controller('addTaskCtrl', function ($scope, $uibModalInstance, Projects, Users, Tasks, CRUD, parent, parentType) {
+	$scope.parent = parent;
+	$scope.parentType = parentType;
 
 	function cleanResponse(resp) {
 		return JSON.parse(angular.toJson(resp));
 	}
 
-	var ids = $scope.project.users;
-	Users.findMany(ids, function(res){
+	var userIds = $scope.parent.users;
+	Users.findMany(userIds, function(res){
 		$scope.users = cleanResponse(res);
 	});
 
@@ -15,26 +16,46 @@ angular.module('taskManagerApp')
 		$uibModalInstance.dismiss();
 	}
 
+	function addTaskToProject(taskId){
+		//add task to project
+		Projects.addTask({
+			projectId: $scope.parent._id,
+			taskId: taskId
+		}, function(){
+			$uibModalInstance.close();
+		});
+	}
+
+	function addSubTaskToTask(){
+
+	}
+
 	$scope.ok = function(){
-		var task = new Tasks({ name: $scope.taskName, description: $scope.taskDescription, users: ids });
+		var selectedUserIds = $scope.users
+		.filter(function(x){return x.selected == true})
+		.map(function(y){return y._id});
+
+		var task = new Tasks({
+			name: $scope.taskName,
+			description: $scope.taskDescription,
+			users: selectedUserIds
+		});
 
 		task.$save(function(){
 			$scope.taskName = '';
 			$scope.taskDescription = '';
 
-			//add task to project
-			Projects.addTask({
-				projectId: $scope.project._id,
-				taskId: task._id
-			}, function(){
-				$uibModalInstance.close();
-			});
+			if($scope.parentType == "project"){
+				addTaskToProject(task._id);
+			} else if ($scope.parentType == "task"){
+				addSubTaskToTask(task._id);
+			}
 
 			//add task to users
 			var allUsers = $scope.users;
 
 			for(var i in allUsers){
-				//if a user id is checked, add the project to that user
+				//if a user id is checked, add the task to that user
 				if(allUsers[i].selected == true){
 					Users.addTask({
 						userId: allUsers[i]._id,
--- public/javascripts/controllers/tasksCtrl.js
+++ public/javascripts/controllers/tasksCtrl.js
@@ -1,75 +1,73 @@
 angular.module('taskManagerApp')
 .controller('tasksCtrl', function ($scope, $uibModal, $rootScope, $routeParams, Projects, Tasks) {
 
-  $rootScope.currentController = 'tasks';
+	$rootScope.currentController = 'tasks';
 
-  function updateTasks(){
-    var project = Projects.get({id: $routeParams.id}, function(){
-      $scope.project = project;
-      $scope.project.date = new Date(project.start_date);
+	function updateTasks(){
+		var project = Projects.get({id: $routeParams.id}, function(){
+			$scope.project = project;
+			$scope.project.date = new Date(project.start_date);
 
-      Tasks.findMany(project.tasks, function(tasks){
-        $scope.tasks = tasks;
-        $scope.dataForTheTree = tasks;
-      });
-    });
-  };
+			Tasks.findMany(project.tasks, function(tasks){
+				$scope.tasks = tasks;
+				$scope.dataForTheTree = tasks;
+			});
+		});
+	};
 
-  updateTasks();
+	updateTasks();
 
-  $scope.addTask = function(){
-    var modalInstance = $uibModal.open({      
-      templateUrl: '/templates/addTaskModal.html',
-      controller: 'addTaskCtrl',
-      size: 'sm',
-      resolve : {
-        currentProject: function(){
-          return $scope.project;
-        }
-      }
-    });
+	$scope.addTaskModal = function(parent, parentType){
+		var modalInstance = $uibModal.open({      
+			templateUrl: '/templates/addTaskModal.html',
+			controller: 'addTaskCtrl',
+			size: 'sm',
+			resolve : {
+				parentType : function(){
+					return parentType
+				},
+				parent: function(){
+					return parent;
+				}
+			}
+		});
 
-    modalInstance.result.then(function(){
-      updateTasks();
-    });
-  };
+		modalInstance.result.then(function(){
+			updateTasks();
+		});
+	}
 
+	$scope.addTask = function(){
+		$scope.addTaskModal($scope.project, "project");
+	}
 
-  $scope.treeOptions = {
-    nodeChildren: "children",
-    dirSelectable: true,
-    injectClasses: {
-      ul: "a1",
-      li: "a2",
-      liSelected: "a7",
-      iExpanded: "a3",
-      iCollapsed: "a4",
-      iLeaf: "a5",
-      label: "a6",
-      labelSelected: "a8"
-    }
-  }
-/*
-  $scope.dataForTheTree =
-  [
-  { "name" : "Joe", "age" : "21", "children" : [
-  { "name" : "Smith", "age" : "42", "children" : [] },
-  { "name" : "Gary", "age" : "21", "children" : [
-  { "name" : "Jenifer", "age" : "23", "children" : [
-  { "name" : "Dani", "age" : "32", "children" : [] },
-  { "name" : "Max", "age" : "34", "children" : [] }
-  ]}
-  ]}
-  ]},
-  { "name" : "Albert", "age" : "33", "children" : [] },
-  { "name" : "Ron", "age" : "29", "children" : [] }
-  ];
-  */
+	$scope.treeOptions = {
+		nodeChildren: "children",
+		dirSelectable: true,
+		injectClasses: {
+			ul: "a1",
+			li: "a2",
+			liSelected: "a7",
+			iExpanded: "a3",
+			iCollapsed: "a4",
+			iLeaf: "a5",
+			label: "a6",
+			labelSelected: "a8"
+		}
+	}
 
-  $scope.fullName = "Adriana Ene";
-  $scope.notNumber = 6;
-  $scope.search = {value : ''};
-  $scope.notifications = [{ value : 'Deadline Task 2 from Project Pizza Shop Online is tomorrow'}
-  ];
+	$scope.fullName = "Adriana Ene";
+	$scope.notNumber = 6;
+	$scope.search = {value : ''};
+	$scope.notifications = [{ value : 'Deadline Task 2 from Project Pizza Shop Online is tomorrow'}
+	];
 
+	$scope.addSubTask = function($event, node){
+		$scope.addTaskModal(node, "task");
+		$event.stopPropagation();
+	}
+
+	$scope.showSelected = function(node){
+		console.log(node);
+	}
 });
