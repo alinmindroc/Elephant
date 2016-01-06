cf4cf766b463f51a0525f48b1f91245dfb021aee
Alin Mindroc <mindroc.alin@gmail.com>
2016-01-06 19:34:29 +0200

    merged project and task lists in profile page in a single table

--- public/javascripts/controllers/profileCtrl.js
+++ public/javascripts/controllers/profileCtrl.js
@@ -1,63 +1,70 @@
 angular.module('taskManagerApp')
 .controller('profileCtrl', function ($scope, $rootScope, $http, $uibModal, Users, Tasks, Projects) {
-  $rootScope.currentController = 'profile';
+	$rootScope.currentController = 'profile';
 
-  $scope.crtUser = Users.get({id: '568cf96c6d42cc1cd4b3b222'}, function(user){
-    $scope.crtProjects = Projects.findMany(user.projects);
-    $scope.crtTasks = Tasks.findMany(user.tasks);
-  });
-
-  $scope.currentProject = "Online Shop Application";
-
-  $scope.language = "English";
-  $scope.notifications = "On";
-  $scope.sharing = "Public";
-
-  $scope.showFilters = false;
-
-  $scope.openDetailsModal = function(){
-    var modalInstance = $uibModal.open({      
-      templateUrl: '/templates/userDetailsModal.html',
-      controller: 'profileDetailsCtrl',
-      resolve: {
-        userData: function(){
-          return {
-            'username': $scope.crtUser.username,
-            'fullName': $scope.crtUser.fullName,
-            'email':    $scope.crtUser.email,
-            'group':    $scope.crtUser.group
-          }
-        }
-      }
+	$scope.crtUser = Users.get({id: '568cf96c6d42cc1cd4b3b222'}, function(user){
+		Projects.findMany(user.projects, function(projects){
+			$scope.crtProjects = projects;
+    	//mapping from project id to project name to use in tasks table
+    	$scope.projectMap = {};
+    	for(var i in projects){
+    		$scope.projectMap[projects[i]._id] = projects[i].name;
+    	}
     });
+		$scope.crtTasks = Tasks.findMany(user.tasks);
+	});
 
-    modalInstance.result.then(function(result){
-      $scope.crtUser.username = result.username;
-      $scope.crtUser.fullName = result.fullName;
-      $scope.crtUser.email = result.email;
-      $scope.crtUser.group = result.group;
-    });
-  }
+	$scope.currentProject = "Online Shop Application";
 
-  $scope.openPreferencesModal = function(){
-    var modalInstance = $uibModal.open({      
-      templateUrl: '/templates/userPreferencesModal.html',
-      controller: 'profilePreferencesCtrl',
-      resolve: {
-        preferencesData: function(){
-          return {
-            'language': $scope.language,
-            'notifications': $scope.notifications,
-            'sharing': $scope.sharing,
-          }
-        }
-      }
-    });
+	$scope.language = "English";
+	$scope.notifications = "On";
+	$scope.sharing = "Public";
 
-    modalInstance.result.then(function(result){
-      $scope.language = result.language;
-      $scope.notifications = result.notifications;
-      $scope.sharing = result.sharing;
-    });
-  }
+	$scope.showFilters = false;
+
+	$scope.openDetailsModal = function(){
+		var modalInstance = $uibModal.open({      
+			templateUrl: '/templates/userDetailsModal.html',
+			controller: 'profileDetailsCtrl',
+			resolve: {
+				userData: function(){
+					return {
+						'username': $scope.crtUser.username,
+						'fullName': $scope.crtUser.fullName,
+						'email':    $scope.crtUser.email,
+						'group':    $scope.crtUser.group
+					}
+				}
+			}
+		});
+
+		modalInstance.result.then(function(result){
+			$scope.crtUser.username = result.username;
+			$scope.crtUser.fullName = result.fullName;
+			$scope.crtUser.email = result.email;
+			$scope.crtUser.group = result.group;
+		});
+	}
+
+	$scope.openPreferencesModal = function(){
+		var modalInstance = $uibModal.open({      
+			templateUrl: '/templates/userPreferencesModal.html',
+			controller: 'profilePreferencesCtrl',
+			resolve: {
+				preferencesData: function(){
+					return {
+						'language': $scope.language,
+						'notifications': $scope.notifications,
+						'sharing': $scope.sharing,
+					}
+				}
+			}
+		});
+
+		modalInstance.result.then(function(result){
+			$scope.language = result.language;
+			$scope.notifications = result.notifications;
+			$scope.sharing = result.sharing;
+		});
+	}
 });
