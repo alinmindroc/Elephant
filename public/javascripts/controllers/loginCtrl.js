be042b3f12300f0a0f70ba8bb8d560adaa386dd6
Alin Mindroc <mindroc.alin@gmail.com>
2016-01-14 00:53:26 +0200

    replaced javascripts alerts with bootstrap alerts

--- public/javascripts/controllers/loginCtrl.js
+++ public/javascripts/controllers/loginCtrl.js
@@ -6,14 +6,25 @@ angular.module('taskManagerApp')
 		$location.url('/profile');
 	}
 
+	$scope.alerts = [];
+
+	$scope.closeAlert = function(index) {
+		$scope.alerts.splice(index, 1);
+	};
+
 	$scope.login = function(){
+		if(!$scope.username || !$scope.password){
+			$scope.alerts.push({ type: 'danger', msg: 'Please fill all fields' });
+			return;
+		}
+
 		var user = {};
 		user.username = $scope.username;
 		user.password = $scope.password;
 
 		Users.login(user, function(res){
 			if(res.error){
-				alert(res.error);
+				$scope.alerts.push({ type: 'danger', msg: res.error });
 			} else {
 				localStorageService.set('loggedUser', res);
 				$location.url('/profile');
--- public/javascripts/controllers/signupCtrl.js
+++ public/javascripts/controllers/signupCtrl.js
@@ -2,34 +2,40 @@ angular.module('taskManagerApp')
 .controller('signupCtrl', function ($scope, $rootScope, localStorageService, Users, $location) {
 	$rootScope.currentController = 'signup';	
 
+	$scope.alerts = [];
+
+	$scope.closeAlert = function(index) {
+		$scope.alerts.splice(index, 1);
+	};
+
 	$scope.signup = function(){
 		if($scope.fullName == '' || $scope.username == '' || $scope.email == ''
 			|| $scope.password == '' || $scope.confirmPassword == ''
 			|| !$scope.fullName || !$scope.username || !$scope.email
 			|| !$scope.password || !$scope.confirmPassword){
-			alert("Please complete all fields!");
-			return;
-		}
-
-		if($scope.password != $scope.confirmPassword){
-			alert("Password Mismatch!");
-			return;
-		}
-
-		var newUser = {};
-		newUser.fullName = $scope.fullName;
-		newUser.username = $scope.username;
-		newUser.email = $scope.email;
-		newUser.password = $scope.password;
-
-		Users.signup(newUser, function(res){
-			if(res.error){
-				alert(res.error);
-			} else {
-				localStorageService.set('loggedUser', res);
-				$location.url('/profile');
-				$rootScope.$emit("setHeaderUser");
-			}
-		});
+			$scope.alerts.push({ type: 'danger', msg: 'Please fill all fields' });
+		return;
 	}
+
+	if($scope.password != $scope.confirmPassword){
+		$scope.alerts.push({ type: 'danger', msg: 'Password mismatch!' });
+		return;
+	}
+
+	var newUser = {};
+	newUser.fullName = $scope.fullName;
+	newUser.username = $scope.username;
+	newUser.email = $scope.email;
+	newUser.password = $scope.password;
+
+	Users.signup(newUser, function(res){
+		if(res.error){
+			$scope.alerts.push({ type: 'danger', msg: res.error });
+		} else {
+			localStorageService.set('loggedUser', res);
+			$location.url('/profile');
+			$rootScope.$emit("setHeaderUser");
+		}
+	});
+}
 });
